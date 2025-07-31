07958879c3736a818b238fef390c842b1d019e13
shirialoni <shirialoni446@gmail.com>
2025-07-31 10:39:44 +0300

    cam run locally fix

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -5,17 +5,15 @@ const ErrorPage = () => {
   const styles = useStyles();
 
   return (
-    <>
-      <Box sx={styles.main}>
-        <Typography variant="h1" sx={styles.header}>
-          Oops!
-        </Typography>
-        <Typography variant="h2">Something went worng</Typography>
-        <Typography variant="h6" marginTop={10}>
-          please try again later, Contact us for help.
-        </Typography>
-      </Box>
-    </>
+    <Box sx={styles.main}>
+      <Typography variant="h1" sx={styles.header}>
+        Oops!
+      </Typography>
+      <Typography variant="h2">Something went wrong</Typography>
+      <Typography variant="h6" marginTop={10}>
+        please try again later, Contact us for help.
+      </Typography>
+    </Box>
   );
 };
 
